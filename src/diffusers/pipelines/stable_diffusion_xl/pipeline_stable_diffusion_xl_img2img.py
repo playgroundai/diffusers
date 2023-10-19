Fix timesteps slice calculation when denoising_start is supplied

src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl_img2img.py
@@ -501,14 +501,16 @@ class StableDiffusionXLImg2ImgPipeline(
         # Strength is irrelevant if we directly request a timestep to start at;
         # that is, strength is determined by the denoising_start instead.
         if denoising_start is not None:
-            discrete_timestep_cutoff = int(
+            num_skip_timesteps = int(
                 round(
-                    self.scheduler.config.num_train_timesteps
-                    - (denoising_start * self.scheduler.config.num_train_timesteps)
+                 (denoising_start * len(timesteps))
                 )
             )
-            timesteps = list(filter(lambda ts: ts < discrete_timestep_cutoff, timesteps))
-            return torch.tensor(timesteps), len(timesteps)
+            if self.scheduler.order == 2:
+                if num_skip_timesteps % 2 == 1:
+                    num_skip_timesteps = num_skip_timesteps - 1
+            timesteps = timesteps[num_skip_timesteps:]
+            return timesteps, len(timesteps)
 
         return timesteps, num_inference_steps - t_start
 