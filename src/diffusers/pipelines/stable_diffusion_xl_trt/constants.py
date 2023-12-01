31478dea5ebf4b1a130004a47429c181921eefef
Tyler Hoyt <tylerhoyt93@gmail.com>
2023-12-01 15:29:11 -0500

    Update constants since I'm using a different server now

--- src/diffusers/pipelines/stable_diffusion_xl_trt/constants.py
+++ src/diffusers/pipelines/stable_diffusion_xl_trt/constants.py
@@ -1,5 +1,6 @@
 ONNX_OPSET = 17
 version = 'xl-1.0'
 
-storage_dir = "/mnt/data/spectral/tyler/"
+# storage_dir = "/mnt/data/spectral/tyler/"
+storage_dir = "/home/ubuntu/tyler/tyler/spectral/optimized_models"
 lora_storage_dir = "/mnt/storage/lora_experiment_model_caches"
