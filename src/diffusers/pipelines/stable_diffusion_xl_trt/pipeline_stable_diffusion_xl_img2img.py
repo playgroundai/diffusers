4df1041aebc89a0e2a89906110ae1bebe64549e7
Finlay Hudson <finlay@spectralcompute.co.uk>
2024-02-12 13:50:53 +0000

    Allow for batched models to work in trt pipelines

--- src/diffusers/pipelines/stable_diffusion_xl_trt/pipeline_stable_diffusion_xl_img2img.py
+++ src/diffusers/pipelines/stable_diffusion_xl_trt/pipeline_stable_diffusion_xl_img2img.py
@@ -187,6 +187,7 @@ class StableDiffusionXLImg2ImgPipeline(
         optimized_model_dir: Optional[str] = None,
         version: Optional[str] = 'xl-1.0',
         pipeline_type: PIPELINE_TYPE = PIPELINE_TYPE.SD_XL_BASE,
+        max_batchsize: int = 1,
     ):
         super().__init__()
 
@@ -216,22 +217,24 @@ class StableDiffusionXLImg2ImgPipeline(
 
         stream = torch.cuda.current_stream().cuda_stream
 
-        self.unetxl_runner = UNETXLRunnerInfer(framework_model_dir=self.optimized_model_dir, version=version, scheduler=None,
-                                          pipeline_type=pipeline_type, stream=stream)
+        self.unetxl_runner = UNETXLRunnerInfer(framework_model_dir=self.optimized_model_dir, version=version,
+                                               scheduler=None, pipeline_type=pipeline_type, stream=stream,
+                                               max_batch_size=max_batchsize)
         self.ref_unetxl_engine = self.unetxl_runner.load_engine()
 
         self.ref_clip_engine = None
         if pipeline_type == PIPELINE_TYPE.SD_XL_BASE:
             clip_runner = CLIPRunner(framework_model_dir=self.optimized_model_dir, output_hidden_states=True,
-                                       version=version,
-                                       pipeline_type=pipeline_type, stream=stream)
-            clip_obj = clip_runner.make_clip()
-            self.ref_clip_engine = clip_runner.load_engine(clip_obj, batch_size=1)
+                                    version=version, pipeline_type=pipeline_type, stream=stream,
+                                     max_batch_size=max_batchsize)
+            self.clip_obj = clip_runner.make_clip()
+            self.ref_clip_engine = clip_runner.load_engine(self.clip_obj)
 
-        clip2_runner = CLIP2Runner(framework_model_dir=self.optimized_model_dir, output_hidden_states=True, version=version,
-                                   pipeline_type=pipeline_type, stream=stream)
-        clip2_obj = clip2_runner.make_clip_with_proj()
-        self.ref_clip2_engine = clip2_runner.load_engine(clip2_obj, batch_size=1)
+        clip2_runner = CLIP2Runner(framework_model_dir=self.optimized_model_dir, output_hidden_states=True,
+                                   version=version, pipeline_type=pipeline_type, stream=stream,
+                                   max_batch_size=max_batchsize)
+        self.clip2_obj = clip2_runner.make_clip_with_proj()
+        self.ref_clip2_engine = clip2_runner.load_engine(self.clip2_obj)
 
         self.vae_runner = ImageOnlyVaeRunner(self.vae)
         self.vae_runner.setup_model()
@@ -241,13 +244,14 @@ class StableDiffusionXLImg2ImgPipeline(
         # The width/height for which the TRT modules are initialised
         self.trt_width = None
         self.trt_height = None
+        self.trt_batch = None
 
         self.unetxl_runner.warmup(self.ref_unetxl_engine, 1024, 1024)
-        self.vae_runner.warmup(1024, 1024, batch_size=1)
+        self.vae_runner.warmup(1024, 1024)
 
         if self.ref_clip_engine is not None:
-            clip_runner.warmup(self.ref_clip_engine, clip_obj)
-        clip2_runner.warmup(self.ref_clip2_engine, clip2_obj)
+            clip_runner.warmup(self.ref_clip_engine, self.clip_obj)
+        clip2_runner.warmup(self.ref_clip2_engine, self.clip2_obj)
 
         torch.cuda.synchronize() # Wait for warmup nonsense to finish.
 
@@ -255,14 +259,20 @@ class StableDiffusionXLImg2ImgPipeline(
     # This operation is very expensive, but less expensive than completely recreating the pipeline. If you have a
     # workload that's mostly images of the same size with the occasional outlier, then exploiting this functionality
     # might be beneficial.
-    def possibly_reinitialise_tensorrt(self, w, h):
-        if self.trt_width == w and self.trt_height == h:
+    def possibly_reinitialise_tensorrt(self, w, h, bs):
+        if self.trt_width == w and self.trt_height == h and self.trt_batch == bs:
             return
 
         self.trt_height = h
         self.trt_width = w
+        self.trt_batch = bs
 
-        self.ref_unetxl_engine.allocate_buffers(shape_dict=self.unetxl_runner.get_shape_dict(h, w), device="cuda")
+        if self.ref_clip_engine is not None:
+            self.ref_clip_engine.allocate_buffers(shape_dict=self.clip_obj.get_shape_dict(bs, h, w),
+                                                  device=self.device)
+        self.ref_clip2_engine.allocate_buffers(shape_dict=self.clip2_obj.get_shape_dict(bs, h, w), device=self.device)
+        self.ref_unetxl_engine.allocate_buffers(shape_dict=self.unetxl_runner.get_shape_dict(h, w, bs),
+                                                device=self.device)
 
         # TODO: this one is delightfully inconsistent.
         # self.base_vae_runner = VAERunner(framework_model_dir=self.optimized_model_dir, version=version,
@@ -1102,6 +1112,23 @@ class StableDiffusionXLImg2ImgPipeline(
         else:
             batch_size = prompt_embeds.shape[0]
 
+        # 4. Preprocess image
+        image = self.image_processor.preprocess(image)
+
+        # Deduce whether the input is a latent or an image. Both are accepted because _reasons_
+        if image.shape[1] == 4:
+            # It's a latent.
+            height, width = image.shape[-2:]
+            height = height * self.vae_scale_factor
+            width = width * self.vae_scale_factor
+        else:
+            height = image.shape[2]
+            width = image.shape[3]
+
+        # Needs to be initialised with the actual *image* size. Note that the "image" parameter may actually
+        # be latents, because everything must be as confusing as possible.
+        self.possibly_reinitialise_tensorrt(width, height, batch_size)
+
         device = self._execution_device
 
         # 3. Encode input prompt
@@ -1128,23 +1155,6 @@ class StableDiffusionXLImg2ImgPipeline(
             lora_scale=text_encoder_lora_scale,
             clip_skip=self.clip_skip,
         )
-
-        # 4. Preprocess image
-        image = self.image_processor.preprocess(image)
-
-        # Deduce whether the input is a latent or an image. Both are accepted because _reasons_
-        if image.shape[1] == 4:
-            # It's a latent.
-            height, width = image.shape[-2:]
-            height = height * self.vae_scale_factor
-            width = width * self.vae_scale_factor
-        else:
-            height = image.shape[2]
-            width = image.shape[3]
-
-        # Needs to be initialised with the actual *image* size. Note that the "image" parameter may actually
-        # be latents, because everything must be as confusing as possible.
-        self.possibly_reinitialise_tensorrt(width, height)
 
         stream = torch.cuda.current_stream().cuda_stream
 
@@ -1285,21 +1295,21 @@ class StableDiffusionXLImg2ImgPipeline(
                 progress_bar.update()
 
         if not output_type == "latent":
-            image = self.vae_runner.run(latents)
+            images = self.vae_runner.run(latents)
         else:
-            image = latents
-            return StableDiffusionXLPipelineOutput(images=image)
+            images = latents
+            return StableDiffusionXLPipelineOutput(images=images)
 
         # apply watermark if available
         if self.watermark is not None:
-            image = self.watermark.apply_watermark(image)
+            images = self.watermark.apply_watermark(images)
 
-        image = self.image_processor.postprocess(image, output_type=output_type)
+        images = self.image_processor.postprocess(images, output_type=output_type)
 
         # Offload all models
         self.maybe_free_model_hooks()
 
         if not return_dict:
-            return (image,)
+            return (images,)
 
-        return StableDiffusionXLPipelineOutput(images=image)
+        return StableDiffusionXLPipelineOutput(images=images)
