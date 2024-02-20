Fix edm scaling with trt vae runner

src/diffusers/pipelines/stable_diffusion_xl_trt/pipeline_playground_v2dot5.py
@@ -1239,7 +1239,7 @@ class PlaygroundV2dot5Pipeline(DiffusionPipeline, FromSingleFileMixin, StableDif
                 edm_std = torch.tensor(self.vae.config.edm_std).view(
                     1, 4, 1, 1).to(latents.device, dtype=latents.dtype)
                 latents_denorm = latents * edm_std / self.vae.config.edm_scale + edm_mean
-                image = self.vae_runner.run(latents_denorm)
+                image = self.vae_runner.run(latents_denorm, latents_already_scaled=True)
 
                 # apply watermark if available
                 if self.watermark is not None:

src/diffusers/pipelines/stable_diffusion_xl_trt/vae.py
@@ -14,11 +14,14 @@ class ImageOnlyVaeRunner:
         self.model.decoder.mid_block.to(dtype)
         self.set_latents_to = next(iter(self.model.post_quant_conv.parameters())).dtype
 
-    def run(self, latents):
+    def run(self, latents, latents_already_scaled = False):
         latents = latents.to(self.set_latents_to)
 
         with torch.no_grad():
-            image = self.model.decode(latents / self.model.config.scaling_factor, return_dict=False)[0]
+            if not latents_already_scaled:
+                latents = latents / self.model.config.scaling_factor
+
+            image = self.model.decode(latents, return_dict=False)[0]
 
         return image
 